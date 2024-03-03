594b51e0db0bfceb86a339145450dff8dc6feed2
Ghulam qadir <154546196+ghulamqadir10@users.noreply.github.com>
2024-03-03 17:20:20 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,123 @@
+// assignment 1
+alert("Error! please enter a valid password")
+alert("Welcome to JS Land..." + "\n" + "Happy Coding!")   //"\n means break aline"
+alert("Welcome to JS Land.." + "\n" + "Happy Coding!" + "\n" +  "Prevent this page from creating additional dialogs")
+
+
+
+// // // // assignment 02
+// // // let username;
+// // // let myname =" ghulam qadir"
+// // // let message="hello"
+// // // message= "Hello World"
+// // // alert(message)
+// // // let student_bio_data;
+// // // name ="Ghulam Qadir"
+// // // age = 20;
+// // Qualification="web and mobile app development"
+// // alert(name)
+// // // alert(age)
+// // // alert(Qualification)
+// // // let who_know_my_pizza;
+// // // who_know_my_pizza="PIZZA" + "\n"+ "PIZZ" +"\n"+ "PIZ" +"\n"+ "PI" +"\n"+ "P"
+// // // alert(who_know_my_pizza)
+// // et Email;
+// //  Email="my Email address is"
+// // // let Email_address;
+// // // Email_address=" example@example.com"
+// // // // let result=Email.concat("",Email_address)
+// // let result=Email.concat(Email_address);
+// // // ale(result)
+// // // let exam;
+// // // exam= "i am trying to learn from the book"
+// // // let book=" A smarter way to learn JavaScript"
+// // // let example=exam.concat(book);
+// // alert(example)
+// document.write("yeah i can write HTML content through java script"+ "<br>" + "<br>" + "<br>")
+// // let an_image = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// // // alert(an_image)
+
+
+
+// // // // let para = document.createElement("h3");
+// // // // let nodes = document.createTextNode(
+// //   "yeah i can write HTML content through java script"
+// // // // );
+// // // para.append(nodes);
+// // // let my_elemen= document.getElementById("mydiv");
+// // // // my_element.appendChild(para);
+// // // // document.getElementById("mydiv").style.border = "10px solid yellow";
+// // // // document.getElementById("mydiv").style.textAlign = "center";
+// // // // document.getElementById("mydiv").style.textTransform = "capitalize";
+
+
+
+
+
+// // // // assignment 03
+// // // let my_age = 20;
+// // // alert(" i am " + my_age + years old");
+// // // // alert("i am 20 years old");
+
+// // // let birth_year;
+// // // birth_year=2003;
+// // // alert(birth_year)
+//  document.write("My birth year is 1990" + "<br>" )
+// document.write("Data type of my  ddeclared variable is number" + "<br>"  + "<br>" +"<br>");
+// // // let visitor_info;
+// // // visitor_name="john Doe";
+// // // product_title= "T-shirt(s)";
+// // // Quantity= 5
+// // console.log(visitor_info)
+
+// document.write("john Doe ordered 5 T-shirt(s) on XYZ Clothing store" + "<br>" + "<br>" + "<br>" + "<br>")
+
+// // // assignment 04
+
+// // counter
+
+// let counter= localStorage.getItem("counter");
+// if (counter === null){
+//     counter=0;
+// }
+// else{
+//     counter= parseInt(counter)
+// };
+// counter++
+//  localStorage.setItem("counter",counter);
+// alert("You have visited" + counter + times);
+
+
+// //  declare 3 variables in one statments;
+
+
+// let student={
+// name: "ghulam qadir",
+// age: 20,
+// institution: "saylani mass it training",
+// };
+// console.log(student);
+
+// // 4 legal and illegal variables;
+
+// let legal1 = "$car"
+// let legal2 = "_car"
+// let legal3 = "car"
+// let legal4 = "$my_div"
+
+
+// // illegal variables
+// let illegal1="#car"
+// let illegal2="@car"
+// let illegal3="#my@div"
+// let illegal4="1num"
+
+
+// // text content to show in browser  
+
+//  document.write("<h2 class='hello'>  Rules for naming JS variables  </h2> " + "<br>" )
+//  document.write("1- variable are only contain $ and _ for example $my_name" + "<br>" )
+//  document.write("2-variable must begin with $,letter or _ . for example $name , _name or name" + "<br>" )
+//  document.write("3- variables are case sensitive" + "<br>" )
+//  document.write("4- variables name should not be js keyword" + "<br>" )
+
